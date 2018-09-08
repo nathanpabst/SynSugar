8befff458b8a788eeb4afd6961153515e20a72f5
unknown <nathan.pabst@gmail.com>
2018-09-07 19:39:35 -0500

    added .gitignore

---
+++ SynSugar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SynSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
